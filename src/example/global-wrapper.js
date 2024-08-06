c66941a937c3139f3a394f0a22b2d059fb0d91f8
Raju Mandal <raju.mandal@combain.com>
2024-08-06 10:47:06 +0530

    Example

--- src/example/global-wrapper.js
+++
@@ -1,4 +0,0 @@
-(function(global) {
-    const AxesModule = require('../../dist/index');
-    global.AxesLayerWithDistance = AxesModule.AxesLayerWithDistance;
-})(typeof window !== 'undefined' ? window : this);
